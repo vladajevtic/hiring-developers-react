bcc800836a0390416b2b500490f6fbd1baa2ff3e
vladajevtic <vladajevtic07@gmail.com>
2022-04-28 17:36:19 +0200

    add acordian to card

--- developers/src/components/Card/Card.component.jsx
+++ developers/src/components/Card/Card.component.jsx
@@ -12,26 +12,56 @@ import style from './Card.module.css'
 import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
 import Divider from '@mui/material/Divider';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function CardComponent(props) {
   const text= props.text;
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
   return (
-    <Card className={style.cardStyle}>
+    <Card>
       <CardHeader
         avatar={
           <Avatar alt={users[0].name} src={users[0].image} className={style.avatarSize} ></Avatar>
         }
         title={
-            users[0].name
+           users[0].name
         }
         subheader={
           users[0].technology
         }
-      />
+        
+        />
       <Divider/>
+      
       <CardContent>
         {text ? <Typography variant="body2" color="text.secondary">
           {users[0].description}
+          <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel4bh-content"
+              id="panel4bh-header"
+            >
+              <Typography>Personal data</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                E-mail:{users[0].email} 
+                <Divider/>
+                Phone: {users[0].phone}
+                <Divider/>
+                Location: {users[0].location}
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
         </Typography> : 
         <Typography variant="body2" color="text.secondary">
           {users[0].description.substring(0,100)}...
@@ -39,17 +69,21 @@ export default function CardComponent(props) {
         
       </CardContent>
       <Divider/>
-      <CardActions sx={{display:"flex", justifyContent: "space-around"}}>
-        <IconButton  className={style.iconButtonSpace} aria-label="add to favorites">
+      <CardActions className={style.cardAction}>
+        <IconButton aria-label="add to favorites">
             <Button color='inherit' startIcon={<FavoriteIcon />}>
             {users[0].rank}
             </Button>
         </IconButton>
-        <Divider orientation="vertical" variant="middle" flexItem />
+        <Divider orientation="vertical"  flexItem />
         <CardContent>
-          <Typography variant="body2" color="text.secondary">price: {users[0].id }$</Typography> 
+          <Typography variant="body2" color="text.secondary">experince: {users[0].experience}</Typography> 
         </CardContent>
-        <Divider orientation="vertical" variant="middle" flexItem />           
+        <Divider orientation="vertical"  flexItem />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">price: {users[0].price }$</Typography> 
+        </CardContent>
+        <Divider orientation="vertical" flexItem />           
         <IconButton  className={style.iconButtonSpace} aria-label="add to favorites">
             <Button component={Link} href={users[0].linkedin} color='inherit'>
               <LinkedInIcon />
@@ -67,5 +101,10 @@ const users= [
       id: 376,
       rank: 5,
       description: "backend developer Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem at, earum odit, quasi enim possimus delectus unde facilis amet optio sunt corporis accusamus repudiandae placeat dignissimos commodi. Ad, eveniet dignissimosOfficiis ducimus assumenda aut possimus nobis autem minima repudiandae, incidunt saepe placeat hic expedita esse cupiditate rerum cumque distinctio commodi necessitatibus molestias quis consequuntur pariatur. Blanditiis placeat quibusdam eum animi.",
-      linkedin: "https://www.linkedin.com"
+      linkedin: "https://www.linkedin.com",
+      experience: "2 years",
+      price: 7,
+      email:  "test@hotmail.com",
+      phone: "060123",
+      location: "Serbia",
   }]
